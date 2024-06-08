6030922916d0cd5805d34a759036a14b0923b62f
Tze Yi Tiong <tytiong2@gmail.com>
2024-06-08 14:47:54 +0800

    configure daisy tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,15 @@ import './App.css'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-red-500 underline text-center">
-      Hello world 2!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold text-red-500 underline text-center">
+        Hello world 2!
+      </h1>
+      <button
+        class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+      </button>
+    </>
   )
 }
 
