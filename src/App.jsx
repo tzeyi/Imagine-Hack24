988500fc41ae51a1c174ca0dc75139b68e4a653c
Tze Yi Tiong <tytiong2@gmail.com>
2024-06-08 14:06:18 +0800

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   return (
     <h1 className="text-3xl font-bold text-red-500 underline text-center">
-      Hello world!
+      Hello world 2!
     </h1>
   )
 }
